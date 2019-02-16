0fe8612c3bae7eaa760e22482d1b6c37987d69dd
Alexey Yarmolovich <ecesilol@gmail.com>
2019-02-16 18:00:00 +0300

    wip

--- UserSession.py
+++ UserSession.py
@@ -2,27 +2,31 @@ import geopy.distance
 
 
 class UserSession:
-    def __init__(self, map_info, user_id, user_location):
+    def __init__(self, map_info, chat_id, user_location):
         self.map_info = map_info
-        self.user_id = user_id
+        self.chat_id = chat_id
         self.last_location = (user_location['longitude'], user_location['latitude'])
 
 
     def start(self, callback):
         cars = self.map_info.get_cars_info()
         self.callback = callback
-        self.find_cars_in_range(self.last_location, 1000, cars)
+        self.find_cars_in_range(self.last_location, 5, cars)
 
 
     def find_cars_in_range(self, user_location, search_range, cars):
         cars_in_range = []
         for car in cars:
-            car_position = ( car['Lon'], car['Lat'])
-            a = geopy.distance.vincenty(user_location, car_position).km
-            if a < search_range:
-                cars_in_range.append(car)
+            car_position = (car['Lon'], car['Lat'])
+            distance = geopy.distance.vincenty(user_location, car_position).km
+            if distance < search_range:
+                cars_in_range.append({'car': car, 'distance': distance})
         if len(cars_in_range) > 0:
-            self.callback(cars_in_range)
-            print('did it sent message?')
+            readable_info = list(map(self.readable_info_for_car, cars_in_range))
+            self.callback(self.chat_id, readable_info)
+            print(readable_info)
 
-
+    def readable_info_for_car(self, car_info):
+        distance = car_info['distance']
+        car = car_info['car']
+        return '{} {} is in {}km'.format(car['Brand'], car['Model'], round(distance, 2))
--- main.py
+++ main.py
@@ -5,42 +5,12 @@ from time import sleep
 from MapInfo import MapTracker
 from UserSession import UserSession
 
-token = "654254537:AAEYgcQt0F0G9UeOMhL77vIo6E5iibnO4cY"
+token = "654254537:AAGtYin0OQ5PRUCY9GdqOdIiU3ICBcKsL-s"
 url = "https://api.telegram.org/bot/"
-
-
-def get_updates_json(urlBase, lastProcessedUpdate):
-    print("get_updates_json start, last was ", str(lastProcessedUpdate))
-    url = urlBase + 'getUpdates?timeout=5&offset=' + str(lastProcessedUpdate + 1)
-    response = requests.get(url)
-    print("get_updates_json finish")
-    return response.json()
-
-
-def last_update(data):
-    results = data['result']
-
-    if len(results) == 0:
-        return None
-    total_updates = len(results) - 1
-    return results[total_updates]
-
-
-def get_chat_id(update):
-    chat_id = update['message']['chat']['id']
-    return chat_id
-
-def check_distance(userLocation):
-    cars_info = requests.get('http://service.drivetime.by/api/cars').json()['cars']
-    print(cars_info)
-
-def send_mess(chat, text):
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
 
 map_info = MapTracker()
 bot = Bot(token)
+current_chat_id = None
 
 def main():
     print("start")
@@ -54,20 +24,21 @@ def main():
     while True:
         sleep(1)
 
-def process_cars(cars_in_range):
-    print("going to send message with", cars_in_range)
-    bot.sendMessage(current_chat_id, 'prepare to cars')
-    bot.sendMessage(current_chat_id, cars_in_range)
+
+current_chat_id = 0
+
+def process_cars(chat_id, cars_in_range):
+    bot.sendMessage(chat_id, 'prepare to cars')
+    bot.sendMessage(chat_id, '\n'.join(cars_in_range))
 
 current_user_id = None
-current_chat_id = None
 
 def handle(message):
-    current_chat_id = message['chat']['id']
+    chat_id = message['chat']['id']
     user_id = message['from']['id']
     if 'location' in message:
         current_user_id = user_id
-        session = UserSession(map_info, user_id, message['location'])
+        session = UserSession(map_info, chat_id, message['location'])
         session.start(process_cars)
     else:
        print('no location found in message', message)
